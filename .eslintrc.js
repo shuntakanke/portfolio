d3cda446b20f200ad025e1e0da3fd0a0d5e30966
菅家俊太 <shunta@MacBook-Pro.local>
2020-06-25 08:55:00 +0900

    second commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": [
-      "prettier",
-    ],
-    "plugins": [
-      "prettier"
-    ],
-    "rules": {
-      "prettier/prettier": ["error", {
-        "singleQuote": true,
-        "bracketSpacing": true,
-        "jsxBracketSameLine": true
-      }]
-    }
-  };
+  parser: 'babel-eslint',
+  extends: ['prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        bracketSpacing: true,
+        jsxBracketSameLine: true
+      }
+    ]
+  }
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import {MediaQueries} from 'vue-media-queries';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { MediaQueries } from 'vue-media-queries';
 
 const mediaQueries = new MediaQueries();
 
@@ -15,4 +15,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
