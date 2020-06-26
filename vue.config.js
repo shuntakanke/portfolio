7274d30a16dd8959bcc54785eea4cb27e3edd214
菅家俊太 <shunta@MacBook-Pro.local>
2020-06-26 19:32:45 +0900

    shunta

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     outputDir: 'docs',
-    assetsDir: './',
-    publicPath: './'
+    publicPath: '/portfolio',
+    filenameHashing: false,
+    productionSourceMap: false,
   }
